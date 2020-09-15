7f03da74fe9c196ea4dbfa30e7f10a5834294063
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-15 04:17:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ require 'pry'
 def reverse_each_word(sentence1)
   # greeting = []
     sentence1.split.collect do |word|
-  greeting << word.reverse 
-  end
-    greeting.join(" ")
+    word.reverse
+  end.join(" ")
 end
